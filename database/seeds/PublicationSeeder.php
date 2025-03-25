a88b0e6524ebb6ffcf0e25b6ed7150a773e57f56
Milene Cavalcante <cavalcantemilene01@alu.ufc.br>
2025-03-25 19:42:00 -0300

    feat: add Text_fieldSeeder file

--- database/seeds/PublicationSeeder.php
+++ database/seeds/PublicationSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class PublicatiomSeeder extends Seeder
+class PublicationSeeder extends Seeder
 {
     /**
      * Run the database seeds.
